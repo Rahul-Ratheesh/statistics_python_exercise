beecbeb70d0a72299a5949e3b22b3ca972582600
Rahul-Ratheesh <rahul.inhive@gmail.com>
2020-01-03 11:51:05 +0530

    Five Number Summary

--- 02_histogram.py
+++ 02_histogram.py
@@ -13,9 +13,12 @@ def importData(filename):
 	return ages
 
 ages = importData(FILENAME)
-ages = sorted(ages)
+figure = pylab.figure()    
+plot = figure.add_subplot(1,1,1)
 print(pylab.hist(ages, bins=11, range=(25, 79), color='steelblue', edgecolor='black', linewidth=0.5))
 pylab.title('Ages of Best Actor Oscar Winners')
 pylab.xlabel('Age')
 pylab.ylabel('Count')
+plot.set_axisbelow(True)
+plot.yaxis.grid(color='gray', linestyle='solid')
 pylab.show()
