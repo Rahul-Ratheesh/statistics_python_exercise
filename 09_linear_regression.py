cd3cebd44e9ae4ee8a83a137ba89d758180c3506
Rahul-Ratheesh <rahul.inhive@gmail.com>
2020-01-04 22:43:13 +0530

    Linear Regression

---
+++ 09_linear_regression.py
@@ -0,0 +1,50 @@
+import pylab, numpy
+
+FILENAME = './data/olympics_2012.csv'
+DELIMITER = ','
+
+def importData(filename):
+	data = {}
+	with open(filename, 'r') as dataFile:
+		headers = dataFile.readline().strip().split(DELIMITER)
+		for header in headers:
+			data[header] = []
+		for line in dataFile:
+			datarecord = line.strip().split(DELIMITER)
+			for index, value in enumerate(datarecord):
+				try:
+					value = int(value)
+				except:
+					data[headers[index]].append(value)
+				else:
+					data[headers[index]].append(value)
+	return data
+
+data = importData(FILENAME)
+# print(data)
+years = data['Year']
+times = [float(time) for time in data['Time']]
+
+years = pylab.array(years)
+# print(years)
+times = pylab.array(times)
+# print(times)
+
+figure = pylab.figure()    
+plot = figure.add_subplot(1, 1, 1)
+plot.set_axisbelow(True)
+plot.yaxis.grid(color='gray', linestyle='solid', alpha=0.5)
+
+pylab.scatter(years, times, color='steelblue')
+pylab.title('Winning times of 1500 meter race in Olympics 2012')
+pylab.xlabel('Year')
+pylab.ylabel('Time')
+a, b = pylab.polyfit(years, times, 1)
+label = f'y = {round(a, 3)}x + {round(b, 3)}'
+estTimes = a * years + b
+pylab.plot(years, estTimes, color='black', label=label, linewidth=1)
+pylab.legend()
+pylab.show()
+
+
+	
