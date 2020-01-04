c69133718b18f49ea5be71c693a3154a8c21beec
Rahul-Ratheesh <rahul.inhive@gmail.com>
2020-01-04 21:55:58 +0530

    Scatterplot

---
+++ 07_scatter_plot.py
@@ -0,0 +1,63 @@
+import pylab, numpy
+
+FILENAME = './data/height.csv'
+DELIMITER = ','
+
+def importData(filename):
+	data = {}
+	with open(filename, 'r') as dataFile:
+		headers = dataFile.readline().strip().split(DELIMITER)
+		for header in headers:
+			data[header] = []
+		for line in dataFile:
+			datarecord = line.strip().split(DELIMITER)
+			for index, value in enumerate(datarecord):
+				try:
+					value = int(value)
+				except:
+					data[headers[index]].append(value)
+				else:
+					data[headers[index]].append(value)
+	return data
+
+def replaceColumn(data, column):
+	data[column] = ['Female' if value == 0 else 'Male' for value in data[column]]
+	return data
+
+
+def groupData(data):
+	print(data.values())
+	record = list(data.values())
+
+	return data
+
+data = importData(FILENAME)
+# print(data)
+data['gender'] = ['Female' if value == 0 else 'Male' for value in data['gender']]
+# print(data)
+
+record = list(data.values())
+male_height, male_weight, female_height, female_weight = [], [], [], []
+for gender, height, weight in zip(record[0], record[1], record[2]):
+	if gender == 'Male':
+		male_height.append(height)
+		male_weight.append(weight)
+	else:
+		female_height.append(height)
+		female_weight.append(weight)		
+
+# heights = data['height']
+# weights = data['weight']
+
+figure = pylab.figure()    
+plot = figure.add_subplot(1, 1, 1)
+pylab.scatter(male_height, male_weight, color = 'blue', label = 'Male')
+pylab.scatter(female_height, female_weight, color = 'red', label = 'Female')
+pylab.title('Height vs Weight')
+pylab.xlabel('Height')
+pylab.ylabel('Weight')
+plot.set_axisbelow(True)
+plot.yaxis.grid(color = 'gray', linestyle = 'solid', alpha = 0.5)
+plot.legend()
+pylab.show()
+
