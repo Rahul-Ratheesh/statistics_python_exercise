1546b4f4d331864a986b262a887d1fb89645b0c1
Rahul-Ratheesh <rahul.inhive@gmail.com>
2020-01-06 12:25:09 +0530

    Creating and Describing Histograms
    Code refactoring

--- 02_histogram.py
+++ 02_histogram.py
@@ -4,18 +4,22 @@ FILENAME = './data/actor_2013.csv'
 DELIMITER = ','
 
 def importData(filename):
+	data = {}
 	with open(filename, 'r') as dataFile:
 		headers = dataFile.readline().strip().split(DELIMITER)
-		ages = []
+		for header in headers:
+			data[header] = []
 		for line in dataFile:
-			dataRecord = line.strip().split(DELIMITER)
-			ages.append(int(dataRecord[0]))
-	return ages
+			datarecord = line.strip().split(DELIMITER)
+			for index, value in enumerate(datarecord):
+				data[headers[index]].append(value)
+	return data
 
 ages = importData(FILENAME)
+ageValues = [int(age) for age in ages['Age']]
 figure = pylab.figure()    
-plot = figure.add_subplot(1,1,1)
-print(pylab.hist(ages, bins=11, range=(25, 79), color='steelblue', edgecolor='black', linewidth=0.5))
+plot = figure.add_subplot(1, 1, 1)
+print(pylab.hist(ageValues, bins=11, range=(25, 79), color='steelblue', edgecolor='black', linewidth=0.5))
 pylab.title('Ages of Best Actor Oscar Winners')
 pylab.xlabel('Age')
 pylab.ylabel('Count')
