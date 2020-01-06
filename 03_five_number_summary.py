70c1a1eadf36e86d6d7c2b6a5d985d6abd4eb101
Rahul-Ratheesh <rahul.inhive@gmail.com>
2020-01-06 12:24:03 +0530

    Interpreting The Five Number Summary
    Refactoring code standardizing the importData function

--- 03_five_number_summary.py
+++ 03_five_number_summary.py
@@ -4,19 +4,23 @@ FILENAME = './data/actor_2013.csv'
 DELIMITER = ','
 
 def importData(filename):
+	data = {}
 	with open(filename, 'r') as dataFile:
 		headers = dataFile.readline().strip().split(DELIMITER)
-		ages = []
+		for header in headers:
+			data[header] = []
 		for line in dataFile:
-			dataRecord = line.strip().split(DELIMITER)
-			ages.append(int(dataRecord[0]))
-	return ages
+			datarecord = line.strip().split(DELIMITER)
+			for index, value in enumerate(datarecord):
+				data[headers[index]].append(value)
+	return data
 
 ages = importData(FILENAME)
+ageValues = [int(age) for age in ages['Age']]
 desc = {}
-desc['min'] = numpy.min(ages)
-desc['Q1']  = numpy.quantile(ages, 0.25)
-desc['median'] = numpy.median(ages)
-desc['Q3']  = numpy.quantile(ages, 0.75)
-desc['max'] = numpy.max(ages)
+desc['min'] = numpy.min(ageValues)
+desc['Q1']  = numpy.quantile(ageValues, 0.25)
+desc['median'] = numpy.median(ageValues)
+desc['Q3']  = numpy.quantile(ageValues, 0.75)
+desc['max'] = numpy.max(ageValues)
 print(desc)
